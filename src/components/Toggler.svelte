58e0fec98a701d617b49738ed23cbc0365a6fe4c
Script Raccoon <scriptraccoon@gmail.com>
2022-04-02 15:09:00 +0200

    use border instead of outline

--- src/components/Toggler.svelte
+++ src/components/Toggler.svelte
@@ -23,23 +23,27 @@
         height: var(--height);
         position: relative;
         border-radius: 100vw;
-        outline: 1px solid #555;
+        border: 1px solid #555;
         display: inline-flex;
         cursor: pointer;
+        box-sizing: content-box;
     }
     .ball {
         position: absolute;
         border-radius: 50%;
         width: var(--height);
         height: var(--height);
+        top: -1px;
         left: 0px;
         transition: left 180ms ease-out, background 200ms ease-out;
         background: linear-gradient(150deg, white, #bbb);
-        outline: 1px solid #555;
+        border: 1px solid #555;
+        box-sizing: content-box;
+        transform: translateX(-1px);
     }
     .toggler.on .ball {
         left: calc(var(--width) - var(--height));
         background: linear-gradient(150deg, black, #555);
-        outline: 1px solid #000;
+        border: 1px solid #000;
     }
 </style>
